22e54e1fd7f92df023270ffeb74d554fd1fcdea7
Oluwatobilob2 <tyme4christ@gmail.com>
2023-07-11 12:13:34 +0100

    Initializing cypress

---
+++ cypress/e2e/logins.cy.js
@@ -0,0 +1,20 @@
+/// <reference types='Cypress'/>
+
+import {logins} from "../fixtures/selectors.js";
+
+describe("Testing login", function () {
+    
+    beforeEach(function () {
+    
+        cy.visit('https://www.konga.com/')
+  
+    });
+    
+    it("LOGIN - I Should be able to sign in my account with valid", function () {
+        cy.get(logins.loginBtn).click()
+        cy.get(logins.emailField).type('jorn1000@yopmail.com')
+        cy.get(logins.passwordField).type('password123')
+        cy.get(logins.signinBtn).click()
+
+    })
+    })
